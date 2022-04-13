7858847fc415e350f5d35f020765b48e74ea4f94
Liam Merritt <lmerrit2@kent.edu>
2022-04-13 14:20:25 -0400

    about first edit - liam

--- src/components/about.js
+++ src/components/about.js
@@ -3,7 +3,14 @@ import React from "react";
 export const About = () => {
   return (
     <>
-      <h1>About</h1>
+      <article>
+        <h1 className="about-header">About</h1>
+        <p>
+          This is a web application created by students in the Modern Web App
+          Development course for the Spring 2022 semester at Kent State
+          University.
+        </p>
+      </article>
     </>
   );
 };
