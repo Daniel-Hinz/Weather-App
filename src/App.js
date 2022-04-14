41a95c633684753ba1a639a911497ce734a9bd55
Daniel Hinz <dhinz1@kent.edu>
2022-04-13 20:48:49 -0400

    added search functionality

--- src/App.js
+++ src/App.js
@@ -1,40 +1,37 @@
 import "./styles/App.css";
-import axios from "axios";
 import { Home } from "./components/home";
 import { AppNav } from "./components/app-nav";
 import { Weekly } from "./components/forecasts/weekly";
 import { Routes, Route } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { About } from "./components/about";
+import axios from "axios";
 
 const key = `${process.env.REACT_APP_WEATHER_API_KEY}`;
-const lat = -41.1456;
-const long = 81.3393;
 
 function App() {
   const [weather, setWeather] = useState();
-  const [weatherData, setWeatherData] = useState([{}]);
-  const [city, setCity] = useState("");
+  const [city, setCity] = useState("Kent");
+  const [lon, setLon] = useState(81.3579);
+  const [lat, setLat] = useState(41.1537);
 
   useEffect(() => {
     const setData = async () => {
       let res = await axios.get(
-        `http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&appid=${key}`
+        `http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${key}`
       );
       setWeather(res.data);
     };
     setData();
-  }, [weatherData]);
+  }, [weather]);
 
-  const submit = async (e) => {
+  const onSubmit = async (e) => {
     e.preventDefault();
-    fetch(
+    let res = await axios.get(
       `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=fahrenheit&appid=${key}`
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        setWeatherData(data);
-      });
+    );
+    setLat(res.data.coord.lat);
+    setLon(res.data.coord.lon);
   };
 
   return weather ? (
@@ -45,14 +42,15 @@ function App() {
             path=""
             element={
               <Home
+                onSubmit={onSubmit}
+                setCity={setCity}
                 weather={weather}
-                weatherData={weatherData}
-                submit={submit}
+                city={city}
               />
             }
           />
           <Route path="about" element={<About />} />
-          <Route path="forecast" element={<Weekly weather={weather}/>} />
+          <Route path="forecast" element={<Weekly weather={weather} />} />
         </Route>
       </Routes>
     </div>
