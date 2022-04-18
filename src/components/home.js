e2ec2975b38f06e57a68ecd1a78e94b0d7348201
Terra Kincy <terramk334@gmail.com>
2022-04-17 20:48:47 -0400

    ente

--- src/components/home.js
+++ src/components/home.js
@@ -16,6 +16,156 @@ const months = [
   "Dec",
 ];
 
+const GetImage = (val) => {
+  console.log(val);
+  switch (val) {
+    case "01d":
+      return (
+        <img
+          className="status-img"
+          src="https://cdn-icons-png.flaticon.com/512/2698/2698194.png"
+          alt="weather icon"
+        />
+      );
+    case "01n":
+      return (
+        <img
+          className="status-img"
+          src="https://cdn-icons.flaticon.com/png/512/1823/premium/1823324.png?token=exp=1650152461~hmac=f1ee028da812ff45d45efe82fef6add0"
+          alt="weather icon"
+        />
+      );
+    case "02d":
+      return (
+        <img
+          className="status-img"
+          src="https://cdn-icons-png.flaticon.com/512/7290/7290574.png"
+          alt="weather icon"
+        />
+      );
+    case "02n":
+      return (
+        <img
+          className="status-img"
+          src="https://cdn-icons-png.flaticon.com/512/5903/5903541.png"
+          alt="weather icon"
+        />
+      );
+    case "03d":
+      return (
+        <img
+          className="status-img"
+          src="https://cdn-icons-png.flaticon.com/512/1779/1779751.png"
+          alt="weather icon"
+        />
+      );
+    case "03n":
+      return (
+        <img
+          className="status-img"
+          src="https://cdn-icons-png.flaticon.com/512/1779/1779751.png"
+          alt="weather icon"
+        />
+      );
+    case "04d":
+      return (
+        <img
+          className="status-img"
+          src="https://cdn-icons-png.flaticon.com/512/494/494435.png"
+          alt="weather icon"
+        />
+      );
+    case "04n":
+      return (
+        <img
+          className="status-img"
+          src="https://cdn-icons-png.flaticon.com/512/494/494435.png"
+          alt="weather icon"
+        />
+      );
+    case "09d":
+      return (
+        <img
+          className="status-img"
+          src="https://cdn-icons.flaticon.com/png/512/3236/premium/3236859.png?token=exp=1650153109~hmac=2a2a7d972925e66880a3754963fea619"
+          alt="weather icon"
+        />
+      );
+    case "09n":
+      return (
+        <img
+          className="status-img"
+          src="https://cdn-icons.flaticon.com/png/512/3236/premium/3236859.png?token=exp=1650153109~hmac=2a2a7d972925e66880a3754963fea619"
+          alt="weather icon"
+        />
+      );
+    case "10d":
+      return (
+        <img
+          className="status-img"
+          src="https://cdn-icons-png.flaticon.com/512/615/615469.png"
+          alt="weather icon"
+        />
+      );
+    case "10n":
+      return (
+        <img
+          className="status-img"
+          src="https://cdn-icons.flaticon.com/png/512/3262/premium/3262956.png?token=exp=1650153257~hmac=ee0f3a85815ecfac836500585fbe2d6a"
+          alt="weather icon"
+        />
+      );
+    case "11d":
+      return (
+        <img
+          className="status-img"
+          src="https://cdn-icons-png.flaticon.com/512/5903/5903459.png"
+          alt="weather icon"
+        />
+      );
+    case "11n":
+      return (
+        <img
+          className="status-img"
+          src="https://cdn-icons-png.flaticon.com/512/5903/5903459.png"
+          alt="weather icon"
+        />
+      );
+    case "13d":
+      return (
+        <img
+          className="status-img"
+          src="https://cdn-icons.flaticon.com/png/512/2315/premium/2315309.png?token=exp=1650153628~hmac=9e41d42fb9bd083d7d329b7259c5d962"
+          alt="weather icon"
+        />
+      );
+    case "13n":
+      return (
+        <img
+          className="status-img"
+          src="https://cdn-icons.flaticon.com/png/512/2315/premium/2315309.png?token=exp=1650153628~hmac=9e41d42fb9bd083d7d329b7259c5d962"
+          alt="weather icon"
+        />
+      );
+    case "50d":
+      return (
+        <img
+          className="status-img"
+          src="https://cdn-icons-png.flaticon.com/512/3313/3313998.png"
+          alt="weather icon"
+        />
+      );
+    case "50n":
+      return (
+        <img
+          className="status-img"
+          src="https://cdn-icons-png.flaticon.com/512/3313/3313998.png"
+          alt="weather icon"
+        />
+      );
+  }
+};
+
 export const Home = ({ weather }) => {
   const [current, setCurrent] = useState(weather.current);
   const [days, setDays] = useState(weather.daily);
@@ -32,7 +182,7 @@ export const Home = ({ weather }) => {
   };
 
   return (
-    <div>
+    <div className="home-container">
       <div className="search-container">
         <form className="search">
           <input
@@ -45,7 +195,7 @@ export const Home = ({ weather }) => {
       </div>
 
       <div className="weekly-comp">
-        <div className="today-details">
+        <div className="today-details flex">
           <div>
             <h1 className="location">Kent, OH</h1>
             <p className="today-status">{current.weather[0].main}</p>
@@ -54,12 +204,8 @@ export const Home = ({ weather }) => {
               {getDirection(current.wind_deg) + " " + current.wind_speed} mph
               Wind
             </p>
-            <img
-              className="status-img"
-              src={`http://openweathermap.org/img/wn/${current.weather[0].icon}.png`}
-              alt="weather icon"
-            />
           </div>
+          <div>{GetImage(current.weather[0].icon)}</div>
         </div>
         <div className="weekly-container">
           {days.map((day, i) =>
@@ -75,11 +221,8 @@ export const Home = ({ weather }) => {
                     <p className="temp">
                       {Math.floor(((day.temp.day - 273.15) * 9) / 5 + 32)}
                     </p>
-                    <img
-                      className="status-img"
-                      src={`http://openweathermap.org/img/wn/${day.weather[0].icon}.png`}
-                      alt="weather icon"
-                    />
+
+                    {GetImage(day.weather[0].icon)}
                   </div>
                 </div>
                 <div className="middle-third">
@@ -88,11 +231,7 @@ export const Home = ({ weather }) => {
                       {Math.floor(((day.temp.max - 273.15) * 9) / 5 + 34)}°
                     </p>
                     <div className="mini-img-container fifty-percent">
-                      <img
-                        className="mini-status-img"
-                        src={`http://openweathermap.org/img/wn/${day.weather[0].icon}.png`}
-                        alt="weather icon"
-                      />
+                      {GetImage(day.weather[0].icon)}
                     </div>
                   </div>
                   <div className="flex">
@@ -100,11 +239,7 @@ export const Home = ({ weather }) => {
                       {Math.floor(((day.temp.min - 273.15) * 9) / 5 + 30)}°
                     </p>
                     <div className="mini-img-container fifty-percent">
-                      <img
-                        className="mini-status-img"
-                        src={`http://openweathermap.org/img/wn/${day.weather[0].icon}.png`}
-                        alt="weather icon"
-                      />
+                      {GetImage(day.weather[0].icon)}
                     </div>
                   </div>
                 </div>
